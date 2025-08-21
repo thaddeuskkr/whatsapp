3aa64695ae6eeecc44be6d5e93e40d4a4a10e5ce
Thaddeus Kuah <tk@tkkr.dev>
2025-08-22 05:02:39 +0800

    feat: add logout route
    Signed-off-by: Thaddeus Kuah <tk@tkkr.dev>

---
+++ src/routes/logout.ts
@@ -0,0 +1,25 @@
+import type { Server } from '../index.ts';
+
+const route = (app: Server) =>
+    app.post('', async ({ status, headers: { authorization }, store: { auth }, whatsapp }) => {
+        if (!authorization && auth.length > 0) {
+            return status(401, { success: false, error: 'No token provided' });
+        }
+
+        if (authorization) {
+            authorization = authorization.split(' ')[1] ?? authorization;
+
+            if (!auth.includes(authorization)) {
+                return status(401, { success: false, error: 'Invalid token' });
+            }
+        }
+
+        await whatsapp.logout();
+        await whatsapp.initialize();
+
+        return {
+            success: true,
+        };
+    });
+
+export default route;
